3434c4bc11432a68e445f72da9ff7b8eb206eed5
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-02-07 10:35:20 +0000

    Update input.py

--- input.py
+++ input.py
@@ -1,9 +1,9 @@
 #Loaded from Mr Reed's github
 print("A simple example of an input statement")
 name = input("Your name? ")
-print("Hi,", name)
+print("Hi," + name)
 
 #Works with numbers too!!
-age = int(input("How old are you? ))
+age = int(input("How old are you? "))
 newAge = age + 1
 print("Next year you will be " + str(newAge) + " years old!")
