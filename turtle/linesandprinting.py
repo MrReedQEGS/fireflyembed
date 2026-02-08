540ea04ecd67d8f35849310d9255058f2893aedb
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-02-08 15:59:08 +0000

    Create linesandprinting.py

---
+++ turtle/linesandprinting.py
@@ -0,0 +1,12 @@
+#Draw some lines and print some things
+#Mr Reed - git hub
+
+#Set up the turtle
+import turtle
+bob = turtle.Turtle()
+bob.speed(1)
+
+bob.forward(50)
+print("done forward")
+bob.left(90)
+print("turned")
