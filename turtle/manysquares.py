106a9ef938e07d8fb4bc62fbe7386e3e1082db98
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-02-08 11:41:35 +0000

    Update manysquares.py

--- turtle/manysquares.py
+++ turtle/manysquares.py
@@ -1,18 +1,18 @@
-#Draw several squares
+#Draw lots of squares
 #Mr Reed - git hub
 
 #Set up the turtle
 import turtle
 bob = turtle.Turtle()
-bob.speed(3)
+bob.speed(7)
 
 #Draw 6 squares
 for j in range(6):    
     for i in range(4):
-        bob.forward(20)
+        bob.forward(10)
         bob.left(90)
 
     #Move along a bit after each square
     bob.penup()
-    bob.forward(10)
+    bob.forward(20)
     bob.pendown()
